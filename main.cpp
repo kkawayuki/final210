aebdea42e184bb817c75d26d953d40d4ee0ae826
kkawayuki <kyukiokawa@gmail.com>
2024-12-09 18:37:27 -0800

    1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <list> //DLL
 using namespace std;
+
+struct coffeeBooth //rename as needed
+{
+    string customerName, drinkOrder; //drinkorder int? 
+    coffeeBooth *next; 
+};
+
+
 
 int main()
 {
-    
+    list<coffeeBooth>coffeeQueue; //central DLL data structure
 }
