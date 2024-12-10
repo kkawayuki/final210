8f64c22fe99238bce06ee317ce05902019b1f014
kkawayuki <kyukiokawa@gmail.com>
2024-12-09 19:32:01 -0800

    6

--- main.cpp
+++ main.cpp
@@ -3,28 +3,46 @@
 #include <array>
 #include <random>
 #include <time.h>
+#include <deque>
 using namespace std;
 
 // global scope arrays with names for customers/drinks, used in generation
 string dNames[] = {"Cappucino", "Iced Coffee", "Pumpkin Spice Latte", "Coffee", "Golden Brew", "Yum Coffee", "Tasty drink with caffine", "expensive drink"};
 string cNames[] = {"Alex", "Jordan", "Taylor", "Morgan", "Casey", "Riley", "Jamie", "Dakota", "Reese", "Avery", "Kevin", "Rowan", "Sawyer", "Blake", "Drew", "Emerson", "Skylar"};
 
-struct customer
+struct customer // parent class
 {
     string customerName;
-    string drinkOrder;
+    string Order;
 
     // default constructor for random value initialization
     customer()
     {
         customerName = cNames[rand() % (cNames->size() - 1)]; // random customername/drinkorder name, -1 to avoid out of bounds behavior
-        drinkOrder = dNames[rand() % (dNames->size() - 1)];
     }
 
     // function to simplify output process
     void printInfo()
     {
-        cout << customerName << ": " << drinkOrder;
+        cout << customerName << ": " << Order;
+    }
+};
+
+struct coffeeCustomer : public customer // use inheritance learned in CS200 to avoid redundancies where possible
+{
+    coffeeCustomer()
+    :customer()
+    {
+        Order = dNames[rand() % (cNames->size() - 1)]
+    }  
+};
+
+struct muffinCustomer : public customer
+{
+    // inherit functionality from customer class
+
+    muffinCustomer()
+    {
     }
 };
 
@@ -36,29 +54,32 @@ int main()
 {
     srand(time(0)); // seed random
 
+    // coffeeShop
     list<customer> coffeeQueue; // central DLL data structure
+    {                           // indent for readability/collapsability in IDE
+        // initialize
+        for (int i = 0; i < 3; i++)
+            coffeeQueue.push_back(customer()); // push back random customer to start
 
-    // initialize
-    for (int i = 0; i < 3; i++)
-        coffeeQueue.push_back(customer()); // push back random customer to start
-
-    cout << "INITIAL LINE: \n";
-    for (auto it : coffeeQueue)
-    {
-        it.printInfo(); // print initial data
+        cout << "INITIAL LINE: \n";
+        for (auto it : coffeeQueue)
+        {
+            it.printInfo(); // print initial data
+            cout << '\n';
+        }
         cout << '\n';
     }
-    cout << '\n';
 
-    // start simulation
-    runDay(10, coffeeQueue);
+    deque<>
+
+        // start simulation
+        runDay(10, coffeeQueue);
 }
 
 // implementation
-void runDay(int days, list<customer> &listCustomers)
+void runDay(int days, list<customer> &listCustomers) // change to accomodate other data structs
 {
     int join = 0; // control to push_front
-    
 
     for (int i = 0; i < days; i++) // for each day you want to sim
     {
@@ -85,9 +106,9 @@ void runDay(int days, list<customer> &listCustomers)
             somethingHappens = true;
         }
 
-        if(!somethingHappens)
+        if (!somethingHappens)
         {
-            cout << "Nothing happened, store empty."
+            cout << "Nothing happened, store empty.\n";
         }
         cout << '\n';
     }
